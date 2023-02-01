34fd46f38643ac2e51169af490039b4702dd6d3a
LuisFranco12 <luisf3525@gmail.com>
2023-02-01 16:54:27 -0500

    created a function that alerts if a player won

--- app.js
+++ app.js
@@ -30,15 +30,28 @@ const winCombinations = [
     [2, 4, 6]
   ];
 
-  // check for a winner
+// check for a winner
 function checkWinner(player) {
     return winCombinations.some((combo) => {
       return combo.every((index) => {
         return cells[index].textContent === player.symbol;
       });
     });
+   }
+
+// outputs the winner or a draw if there is no winner
+function winner(player) {
+    setTimeout(() => {
+      if (draw(player1, player2)) {
+        alert("Draw");
+        reset();
+      }
+      if (checkWinner(player)) {
+        alert(`Player ${player.num} Wins!`);
+        reset();
+      }
+    }, 200);
   }
-  
 
 function game(e) {
     if (e.target.textContent) return;
