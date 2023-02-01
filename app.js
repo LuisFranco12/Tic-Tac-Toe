66249dfd97d20ec42c9c68b6c39b6cdd199b8b32
LuisFranco12 <luisf3525@gmail.com>
2023-02-01 16:36:44 -0500

    made a function that gives each player a turn to play

---
+++ app.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const cells = document.querySelectorAll(".cell");
+
+function game(e) {
+    if (e.target.textContent) return;
+    if (turn) {
+      e.target.textContent = player1.symbol;
+      turn = !turn;
+    } else {
+      e.target.textContent = player2.symbol;
+      turn = !turn;
+    }
+  }
