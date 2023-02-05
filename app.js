bc835ec5008ee01073155a1edbe36c485380675d
NEEDF0sp33d <luisf3525@gmail.com>
2023-02-05 11:48:49 -0500

    fixed structure of the code and add more to the README file

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const oScore = document.getElementById("o-score");
 let turn = true;
 
 class Player {
-    constructor(num, symbol) {
-        this.num = num;
-        this.symbol = symbol;
-        this.score = 0;
-    }
+ constructor(num, symbol) {
+    this.num = num;
+    this.symbol = symbol;
+    this.score = 0;
+ }
 }
 
 const player1 = new Player(1, 'X');
@@ -36,77 +36,77 @@ const winCombinations = [
 ];
 
 function reset() {
-    cells.forEach((cell) => {
-        cell.textContent = "";
-        cell.classList.remove("p1", "p2");
-    });
-    turn = true;
+  cells.forEach((cell) => {
+      cell.textContent = "";
+      cell.classList.remove("p1", "p2");
+  });
+  turn = true;
 }
 
 // check for a winner
 function checkWinner(player) {
-    return winCombinations.some((combo) => {
-      return combo.every((index) => {
-        return cells[index].textContent === player.symbol;
-      });
-    });
+  return winCombinations.some((combo) => {
+    return combo.every((index) => {
+      return cells[index].textContent === player.symbol;
+     });
+  });
 }
 
 // checks for a draw
 function draw(p1, p2) {
-    return winCombinations.every((combo) => {
-      return combo.every((index) => {
-        return (
-          cells[index].textContent === p1.symbol ||
-          cells[index].textContent === p2.symbol
-        );
-      });
+  return winCombinations.every((combo) => {
+    return combo.every((index) => {
+      return (
+        cells[index].textContent === p1.symbol ||
+        cells[index].textContent === p2.symbol
+      );
     });
+  });
 }
 
 
 // outputs the winner or a draw if there is no winner
 function winner(player) {
-    setTimeout(() => {
-      if (checkWinner(player)) {
-        alert(`Player ${player.num} Wins!`);
-        reset();
-      }
+  setTimeout(() => {
+    if (checkWinner(player)) {
+      alert(`Player ${player.num} Wins!`);
+      reset();
+    }
 
-      if (draw(player1, player2)) {
-        alert("Draw");
-        reset();
-      }
-    }, 200);
+    if (draw(player1, player2)) {
+      alert("Draw");
+      reset();
+    }
+  }, 200);
 }
 
 // updates the score
 function updateScore() {
-    if (checkWinner(player1)) {
-      player1.score++;
-      xScore.textContent = player1.score;
-    }
-    if (checkWinner(player2)) {
-      player2.score++;
-      oScore.textContent = player2.score;
-    }
+  if (checkWinner(player1)) {
+    player1.score++;
+    xScore.textContent = player1.score;
+  }
+  if (checkWinner(player2)) {
+    player2.score++;
+    oScore.textContent = player2.score;
+  }
 }
 
   // game logic
 function game(e) {
-    if (e.target.textContent) return;
-    if (turn) {
-      e.target.textContent = player1.symbol;
-      e.target.classList.add("p1");
-      turn = !turn;
-    } else {
-      e.target.textContent = player2.symbol;
-      e.target.classList.add("p2");
-      turn = !turn;
-    }
-    winner(player1);
-    winner(player2);
-    updateScore()
+  if (e.target.textContent) return;
+  if (turn) {
+    e.target.textContent = player1.symbol;
+    e.target.classList.add("p1");
+    turn = !turn;
+  } else {
+    e.target.textContent = player2.symbol;
+    e.target.classList.add("p2");
+    turn = !turn;
+  }
+  winner(player1);
+  winner(player2);
+  updateScore()
 }
 
 container.addEventListener("click", game);
